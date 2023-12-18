c3e2b05b2cb5b2aa8c58f915a299778ab07e8e22
Nikolaos Pyrgiotis <nickpyrgio@gmail.com>
2023-12-18 19:27:50 +0200

    Added vagrant-ansible-provision.conf.rb, updated vagrant-ansible-provision.conf.rb.dist with $ENVIRONMENT_FILE variable

---
+++ libvirt/vagrant-ansible-provision.conf.rb
@@ -0,0 +1,3 @@
+$LAB="libvirt"
+$SETTINGS_FILE="#{VAGRANTFILE_DIR}/../servers.yml"
+$ENVIRONMENT_FILE="#{VAGRANTFILE_DIR}/../environment.yml"
---
+++ webapp/vagrant-ansible-provision.conf.rb
@@ -0,0 +1,4 @@
+$LAB="webapp"
+$SETTINGS_FILE="#{VAGRANTFILE_DIR}/../servers.yml"
+$ENVIRONMENT_FILE="#{VAGRANTFILE_DIR}/../environment.yml"
+
